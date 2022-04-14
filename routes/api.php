dd1243c7c0654b5c3d70b19a5c72d610c9e24210
omar11111 <algalfy71@gmail.com>
2022-04-14 22:55:20 +0200

    passport package

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->prefix('v1')->group( function () {
+   Route::get('/user',function(Request $request){
+    return $request->user();
+   });
+});
+Route::get('/test',function(Request $request){
+    return 'Authentication';
+});
